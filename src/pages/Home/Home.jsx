3ec0f66af17629a909c5d9bb1ef0c07f304ed562
Sabba Hossain <sabbahossain@gmail.com>
2025-11-20 13:13:18 +0600

    about add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <h1 className='text-center text-3xl uppercase'>welcome to our home page</h1>
-
+  
             <Hero/>
         </div>
     );
