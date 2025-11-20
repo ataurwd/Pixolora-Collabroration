d1ca3f7e73bf63ce01e60aee8f207d5914df5b5d
ataurwd <ataurrahman24707@gmail.com>
2025-11-20 10:46:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center'>Hello, React Router!</h1>
+    </div>
+  );
+}
+
+export default App;
