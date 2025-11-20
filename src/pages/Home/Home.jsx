4d327a8af0a61c71a7ddd5d91bc5bcab8a8896c2
Sabba Hossain <sabbahossain@gmail.com>
2025-11-20 13:12:00 +0600

    about add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../../Home/Hero';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-center text-3xl uppercase'>welcome to our home page</h1>
+
+            <Hero/>
         </div>
     );
 };
