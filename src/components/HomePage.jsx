186c2ba39e91c762737d0d3c2c5f22bbbe8e2dab
ataurwd <ataurrahman24707@gmail.com>
2025-11-20 11:07:29 +0600

    i added npm helmate to change dynamic website title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Home page Design</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>My React App</title>
+        <meta name="description" content="This is my React application." />
+      </Helmet>
+      <h1>Home page Design</h1>
+    </div>
+  );
 };
 
 export default HomePage;
